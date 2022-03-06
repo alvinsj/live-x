b89675728e7de2e2aac6f5d64d65c38314ff7622
alvinsj <text@alvinsj.net>
2022-03-06 22:52:47 +0100

    Fix linting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    dirs: ['pages','entities', 'utils', 'hooks',  'services'],
+  },
 }
 
 module.exports = nextConfig
